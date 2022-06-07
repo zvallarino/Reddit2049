6477f2c3d009a9515ae4d92064045d9f164d87f0
zachary vallarino <zvallarino@gmail.com>
2022-06-06 20:30:58 -0400

    working

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,17 +1,31 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
-import { GET_ALL_POSTS } from '../graphql/queries'
+import { GET_ALL_POSTS, GET_ALL_POSTS_BY_TOPIC } from '../graphql/queries'
 import Post from './Post';
+import TimeAgo from 'javascript-time-ago'
 
-function Feed() {
-  const {data, error} = useQuery(GET_ALL_POSTS)
+import en from 'javascript-time-ago/locale/en.json'
+import ru from 'javascript-time-ago/locale/ru.json'
+
+TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(ru)
+
+type Props = {
+  topic?:string
+}
+
+
+function Feed({topic}: Props) {
+  const {data, error} = !topic? useQuery(GET_ALL_POSTS) : useQuery(GET_ALL_POSTS_BY_TOPIC)
 
   const posts: Post[] = data?.getPostList;
 
   return (
-    <div>{posts?.map(post =>
+    <div className="mt-5 space-y-4">
+      {posts?.map(post =>
       <Post key ={post.id} post ={post}/>
-      )}</div>
+      )}
+      </div>
   )
 }
 
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/outline'
+import { ArrowDownIcon, ArrowUpIcon, BookmarkIcon, ChatAltIcon, DotsHorizontalIcon, GiftIcon, ShareIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Avatar from './Avatar'
 import ReactTimeAgo from 'react-time-ago'
@@ -25,17 +25,49 @@ function Post({post}:Props) {
           <div className="flex items-center space-x-2">
             <Avatar seed ={post.subreddit[0]?.topic}/>
             <p className = "text-xs text-gray-400">
-              <span className = 'font-bold text-black hover:text-blue-400 hover:underline '>/r{post.subreddit[0]?.topic}</span> · Posted by {post.username}
+              <span className = 'font-bold text-black hover:text-blue-400 hover:underline '>r/{post.subreddit[0]?.topic}</span> · Posted by {post.username}
               <ReactTimeAgo date = {new Date(post.created_at)} locale="en-US" />
             </p>
             
           </div>
           
           {/* //Body */}
+          <div className = "py-4"> 
+            <h2 className = "text-xl font-semibold">{post.title}</h2>
+            <p className = "mt-2 text-sm font-light">{post.body}</p>
+          </div>
 
           {/* //Image */}
+          <img className = "w-full" src={post.image} alt = ""/>
 
           {/* //Footer */}
+          <div className = "flex space-x-4 text-gray-400">
+            <div className = "postButtons">
+              <ChatAltIcon className = "h-6 w-6" />
+              <p className = "">{post.comments.length} Comments</p> 
+            </div>
+
+            <div className = "postButtons">
+              <GiftIcon className = "h-6 w-6" />
+              <p className = "hidden sm:inline"> Award</p> 
+            </div>
+
+            <div className = "postButtons">
+              <ShareIcon className = "h-6 w-6" />
+              <p className = "hidden sm:inline"> Share</p> 
+            </div>
+
+            <div className = "postButtons">
+              <BookmarkIcon className = "h-6 w-6" />
+              <p className = "hidden sm:inline"> Save</p> 
+            </div>
+
+            <div className = "postButtons">
+              <DotsHorizontalIcon className = "h-6 w-6" />
+            </div>
+
+
+          </div>
       </div>
    </div>
   )
