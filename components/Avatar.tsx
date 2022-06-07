d99e4221094f702fa972b573def8630ff9eca19c
zachary vallarino <zvallarino@gmail.com>
2022-06-07 15:49:14 -0400

    weird image issue

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -14,7 +14,7 @@ function Avatar({seed, large}: Props) {
     <div className={`relative overflow-hidden h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20'}`}>
       <Image 
       layout="fill"
-      src = {`https://avatars.dicebear.com/api/open-peeps/${seed||session?.user?.name||'placeholder'}.svg`} />
+      src = {`https://avatars.dicebear.com/api/open-peeps/${session?.user?.name||'placeholder'}.svg`} />
     </div>
   )
 }
